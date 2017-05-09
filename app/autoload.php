3ba5885fe05d6eb9fa7979c258cee66e9e51b36c
Dean3 <Hqh2z8hw>
2017-05-09 10:40:41 +0200

    test weg

--- app/autoload.php
+++ app/autoload.php
@@ -4,7 +4,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /** @var ClassLoader $loader */
-//test
+
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
